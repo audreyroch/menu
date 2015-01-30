2bd489b1bfcb69f5577ca53c6ee8c8e9b7b70fe2
root <root@ns313780.ip-188-165-231.eu>
2015-01-30 16:55:40 +0100

    initialise github depot

---
+++ app/code/local/Dedaluvia/Menu/Block/Adminhtml/Extensioninfo.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Fooman Common
+ *
+ * @package   Fooman_Common
+ * @author    Kristof Ringleff <kristof@fooman.co.nz>
+ * @copyright Copyright (c) 2012 Fooman Limited (http://www.fooman.co.nz)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class Dedaluvia_Menu_Block_Adminhtml_Extensioninfo extends Mage_Adminhtml_Block_System_Config_Form_Field
+{
+    
+}
---
+++ app/code/local/Dedaluvia/Menu/Model/Selftester/Abstract.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Fooman Common
+ *
+ * @package   Fooman_Common
+ * @author    Kristof Ringleff <kristof@fooman.co.nz>
+ * @copyright Copyright (c) 2012 Fooman Limited (http://www.fooman.co.nz)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class Dedaluvia_Menu_Model_Selftester_Abstract extends Mage_Core_Model_Abstract
+{
+
+
+
+}
